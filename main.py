cc7094f0c52340dc4eb68011401cbff2bc3768ed
Leia Naomi Novales <leianaominovales@iskolarngbayan.pup.edu.ph>
2026-05-15 22:10:26 +0800

    Update UML diagram and Python logic

--- main.py
+++ main.py
@@ -5,35 +5,65 @@ UI/UX - Anika Almonia
 QA Testing - Carelle Jumao-As
 Software Engineer - Francheska Allan
 Software Engineer - Archie Gujilde
-Software Engineer - Josh Brian Las-Marias"""
+Software Engineer - Brian Josh Las-Marias"""
 
-#Priority
+# Priority - Constants
 class PriorityLevel:
     CRITICAL = "Critical"
     MAJOR = "Major"
     MINOR = "Minor"
 
-#Status of the Report
+# Status of the Report
 class ReportStatus:
+    SUBMITTED = "Submitted"
+    REVIEWED = "Reviewed"
+    ASSIGNED = "Assigned"
     PENDING = "Pending"
+    REJECTED = "Rejected"
     IN_PROGRESS = "In Progress"
     RESOLVED = "Resolved"
-    REJECTED = "Rejected"
 
 # Parent Class
 class User: # tentative
     def __init__(self, user_type):
         self.user_type = user_type
 
+# Resident Class
 class Resident(User): 
-    def __init__(self, concern = "", severity = ""):
+    def __init__(self, name, location, concern = "", severity = ""): #time
+        self.name = name
+        self.location = location
         super().__init__("Resident")
         self.concern = concern 
         self.severity = severity
     
-    def report_concern(self):
-        pass
+    # Input for Concerns
+    def report_concern(self) -> str: #method
+        self.concern = input("What's your concern? ")
+        self.severity = input("What is the severity of the concern (Minor, Major, Critical)")
+        self.display_hotlines()
+        return ReportStatus.SUBMITTED  # Automatically submits the report after the concern is reported
+        
+    # Hotlines Depending on Severity of Concern
+    def display_hotlines(self) -> str: 
+        if self.severity == "Minor":
+            barangay = "Barangay Hotline: (02) 0000-0000"
+            city_hall = "City Hall: 234-5678"
+            print(f"Contact the corresponding hotlines:\n{barangay}\n{city_hall}")
+        elif self.severity == "Major":
+            police = "Police Non Emergency: 345-6789"
+            fire_dept = "Fire Department(Sta.Mesa): (02) 8716-1634/0917-834-0309"
+            print(f"Contact the corresponding hotlines:\n{police}\n{fire_dept}")
+        elif self.severity == "Critical":
+            print("Contact this immediately.")
+            national = "National Emergency Hotline: 911"
+            ndrrmc = "NDRRMC: (02) 8911- 55061 to 65 (local 100),\
+                (02) 8911-1406, (02) 8912-2665, (02) 8912-5668, or (02) 8911-1873"
+            print(f"{national}\n{ndrrmc}")
+        print("Concern/s submitted to the Government Officials.")
 
+
+# Admin Class
 class Admin(User):
     #Encapsulation Private (Security Numbers)
     _sec_number_map = {"11111A": PriorityLevel.MINOR, "12121B": PriorityLevel.MAJOR, "13131C": PriorityLevel.CRITICAL}
